14ef5d64fb37884e3cf8f7101d8ffdc0bd236985
PhgX <phuongbm211@gmail.com>
2022-05-01 10:13:22 +0700

    up folder test1

---
+++ index.js
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="https://glitch.com/favicon.ico" />
+	<title>Học code html</title>
+</head>
+<body>
+	<center>
+        <h1 style ="background-color: powderblue; font-size: 500%;">	
+	       Phương học code HTML 
+        </h1>
+    </center>
+    <hr>
+    
+	<p style="text-align: center;"> 
+        Hôm nay mình học được một chút
+    </p>
+	
+    <center>
+        <p style="color: darkred;"> Dòng này màu đỏ
+        </p>
+    </center>
+	
+    <p style="background: chocolate;">
+        Nền dòng này màu chocolate
+    </p>
+	
+    <p style="font-family: courier;">
+        Font chữ dòng này là courier
+    </p>
+	
+    <p style="font-family: nevada;">
+        Font dòng này là nevada
+    </p>
+	
+    <p style="text-align: right;"> 
+        Dòng này nằm bên phải
+    </p>
+	
+    <p style="font-size: 400%;">
+        Dòng này to gấp 4 lần dòng trên
+    </p>
+	
+    <a href="mailto:lala@gmail.com">
+        Click vô đây để gửi mail
+    </a>
+    
+	<img src="https://1.bp.blogspot.com/-b7lH4n8erIc/Xlk9NaZ867I/AAAAAAAAAKY/fZwkni5uemY0kxptTsavpncA9wFEILf4QCLcBGAsYHQ/s640/T%25E1%25BB%2591i%2B%25C6%25B0u%2B%25E1%25BA%25A3nh%2Bcho%2Bbai%2Bvi%25E1%25BA%25BFt.jpg" alt="Ảnh lỗi" style="width:400px;height:500px;">
+	
+    <p>
+        <img src="https://glitch.com/favicon.ico" alt="Ảnh lỗi" style="float:right; width: 50px; height: 50px;">Ảnh nổi bên phải
+    </p>
+	
+    <p>
+        <img src="https://glitch.com/favicon.ico" alt="Lỗi" style="float: left; width: 70px; height: 70px;">Ảnh nổi bên trái
+    </p>
+	
+    <a href="https://truyenfull.vn">
+        Vô đây để đọc truyện
+    </a>
+	
+    <h1 style="font-size: 60px;">
+        Nhấn vào ảnh dưới để đọc truyện tranh
+    </h1>
+	
+    <a href="https://nettruyen.com" target="_blank">
+        <img src="https://thinhweb.com/wp-content/uploads/2016/03/logo.png" alt="Lỗi" style="width: 200px; height: 200px; ">
+    </a>
+	
+    <a href="https://dantri.vn" target="_blank">
+        <img src="https://thinhweb.com/wp-content/uploads/2016/03/logo.png" alt="Lỗi" style="width: 200px; height: 200px; ">
+    </a>
+	
+    <a href="https://quantrimang.com" target="_blank">
+        <img src="https://o.rada.vn/data/image/2021/01/21/top-trang-web-tai-anh-png-trong-suot-700.jpg" alt="Lỗi" style="width: 100px; height: 100px;">
+    </a>
+	
+    <h1 style="font-size: 200%">
+        Nhấn vào ảnh dưới
+    </h1>
+    
+    <a href="https://dantri.vn" target="_blank">
+        <img src="https://glitch.com/favicon.ico" alt="Lỗi" style="width: 48px; height: 48px;">
+    </a>
+    
+    <button onclick="document.location='http://dantri.vn'">
+        Ấn vào nút này
+    </button>
+    
+    <p>
+        Đoạn văn bản sau đây được giữ nguyên khoảng space và ngắt dòng
+    </p>
+    
+    <pre>
+        <p style="font-size: 300%;">
+            Một đoạn văn bản
+            Hai đoạn văn bản
+            Ba
+            Bốn
+            Năm
+            Sáu
+        </p>
+    </pre>
+    
+    <h1>
+        Đoạn văn bản dưới được căn chỉnh dạng "justify"
+    </h1>
+    
+    <p style="text-align: justify;">
+        Có thể nói trong những năm gần đây thì ngôn ngữ Javascript đã trở nên rất thông dụng. Có rất nhiều framework ra đời được viết bằng ngôn ngữ lập trình Javascript, từ frontend cho tới backend thì ở đâu cũng có mặt nó.
+    </p>
+        
+    <h1>
+        <center>
+            <b>HTML text fomatting</b>
+        </center>
+    </h1>
+    
+    <p>
+        <b>Trong bài</b> này 
+        <strong>chúng ta</strong> 
+        <i>sẽ tìm hiểu khái niệm</i> 
+        <em>Javascript là gì</em> ,
+        <small>và những công nghệ</small>
+        <s>/ Javascript</s><big> Framework</big>
+        <ins>phổ biến nhất hiện nay.</ins><br />
+        <mark>Có thể nói trong những năm gần đây thì ngôn ngữ Javascript đã trở nên rất thông dụng. Có rất nhiều framework ra đời được viết bằng ngôn ngữ lập trình Javascript, từ frontend cho tới backend thì ở đâu cũng có mặt nó.</mark><br />
+        <sub>Vì một số bạn không hiểu rõ về Javascript nên trong bài này <u>mình sẽ tóm tắt khái niệm một cách tông quát nhất</u></sub>
+        <sup> qua đó sẽ giúp bạn hiểu công dụng thực sự của Javascript.</sup>
+    </p>
+
+    <blockquote cite="http://dantri.vn"> For 50 years, WWF has been protecting the future of nature. The world's leading conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United States and close to 5 million globally.
+    </blockquote><br>
+        <cite>Đồi gió hú</cite> <br>
+        <abbr title="Bùi Minh Phương">PhgX</abbr><br>
+        <q>hehe</q>
+        <address>Nhà ở Đông Lào<br>
+                 SĐT: ....00<br>
+                 Mail:....@gmail.com<br></address>
+        <bdo dir="rtl">Viết từ phải sang trái</bdo>
+
+    
+    <hr>
+    <center><h1 style="font-size: 500%">Phần này là JavaScript</h1></center>
+    <p id="demo">JavaScript can change HTML content.</p>
+    <button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+    <div style="background-image:url('linear-icon-48px-24-search-n-16.png')">
+        <p>DÒng thứ 1</p>
+        <p> Dòng thứ 2</p>
+    </div>
+    <div style="background-color: cadetblue;">
+        <p>Target element :<br>
+        <a href="https://www.w3schools.com" rel="author" hreflang="en" target="_blank" referrer-policy="">Ấn vô mở tab mới</a><br>
+        <a href="https://www.w3schools.com" target="_self">Ấn vô self</a><br />
+        <a href="https://www.w3schools.com" target="_parent">Ấn vô parent</a><br />
+        <a href="https://www.w3schools.com" target="_top">Ấn vô top</a><br />
+        </p>
+    </div>
+    <div id="sidebar"; style="background-color: aqua">
+        <p>Đây là phần sidebar</p>
+    </div>
+    <div id="footer"; style="background-color: yellow">
+        <p>Đây là phần footer</p>
+    </div>
+    
+    <div>
+        <label>Hôm nay mua gì</label>
+        <input type="radio" name="mua">Gà <input type="radio" name="mua">Rau
+    </div>
+        
+    <div>
+        <label>Bạn có đủ tiền đóng học phí không</label>
+        <select>
+            <option>Có</option>
+            <option>Không</option>
+        </select>
+    </div>
+    
+</body>
+</html>
